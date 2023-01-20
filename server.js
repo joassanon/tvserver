11f5d91701f0bac86a51b100310d5f60d3decea8
flashtvhaiti <FLASHTVHAITI@GMAIL.COM>
2023-01-20 15:18:50 -0800

    Server

---
+++ server.js
@@ -0,0 +1,37 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    mediaroot: './media',
+    allow_origin: '*'
+  },
+//   auth: {
+//     play: true,
+//     publish: true,
+//     secret: 'nodemedia2017privatekey'
+//   },
+//   trans: {
+//     ffmpeg: '/usr/local/bin/ffmpeg',
+//     tasks: [
+//       {
+//         app: 'live',
+//         mp4: true,
+//         mp4Flags: '[movflags=frag_keyframe+empty_moov]',
+//       }
+//     ]
+//   }
+};
+
+var nms = new NodeMediaServer(config)
+nms.on('preConnect',(id,StreamPath,args) => {
+    console.log(id,StreamPath,args)
+})
+nms.run();
